cd5129673564efcfe50c0ccd29d73103f7b1adb3
Emilia Gromulska <emgromulska@gmail.com>
2020-04-23 12:29:46 +0200

    tests update

--- src/main/java/apiWeatherProject/Config.java
+++ src/main/java/apiWeatherProject/Config.java
@@ -2,6 +2,6 @@ package apiWeatherProject;
 
 public class Config {
     //TODO uzupe�ni�
-    public static final String APP_ID = "f16898b1453340edd5123d717f02a81c";
+    public static final String APP_ID = "your_id";
     public static final String APP_URL = "http://api.openweathermap.org/data/2.5/weather";
 }
